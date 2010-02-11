8b7d62e0c22e8b3ccc459f601f40480dc0289a2d
bale.prasanna <bale.prasanna@ecc37590-14c7-11df-9524-c76eb015ffed>
2010-02-11 16:18:48 +0000

    Added IITAA ruleset. changes made: AInspector/chrome/content/ainspector/OAA/rulesets/wcag20-ruleset.js AInspector/chrome/content/ainspector/OAA/nls/wcag20-ruleset_en-us.js

--- trunk/chrome/content/ainspector/OAA/rulesets/wcag20-ruleset.js
+++ trunk/chrome/content/ainspector/OAA/rulesets/wcag20-ruleset.js
@@ -153,7 +153,7 @@ OpenAjax.a11y.addRuleset(
 		       		'siblingDupName' : {severityCode:'level.PotentialViolation', messageCode:'headers.msg.siblingDupName'},
 		       		'noHeaders' : {severityCode:'level.PotentialViolation', messageCode:'headers.msg.noHeaders'}
 			    }
-			}, // 3.1.1 
+			}, // 2.4.6 
 			
             {
                 criterionNumber : '3.1.1',
@@ -206,6 +206,53 @@ OpenAjax.a11y.addRuleset(
 	        
 	        requirements : [ 
 	        {
+                criterionNumber : '1.1',
+                criterionLevel : 'IITAA10.level.A',
+                criterionDesc : 'IITAA10.description.1_1',
+                requirementUrl: '#text-equiv',
+		        rules : {
+		            // defined in text-equiv-rules.js    
+		    	    'imgAltAttrMissing' : {severityCode:'level.violation', messageCode:'images.msg.altmissing'},
+		            'imgAltAttrWithForebiddenFileExt' : {severityCode:'level.violation', messageCode:'images.msg.althasfileext'},
+		            'imgAltAttrWithForebiddenWord' : {severityCode:'level.violation', messageCode:'images.msg.altforbidenword'},
+		            'imgAltAttrLength' : {severityCode:'level.potentialViolation', messageCode:'images.msg.altbadlen'},
+		            'imgAltLongdescInvalid' : {severityCode:'level.potentialViolation', messageCode:'images.msg.invalidLongdesc'},
+		            'imgPresentationalWithAltOrTitle' : {severityCode:'level.recommendation', messageCode:'images.msg.imgPresentationalWithAltOrTitle'},
+		            'imgAltAttrEmptyTitleAttrPresent' : {severityCode:'level.recommendation', messageCode:'images.msg.altother'}
+            	}
+            }, // 1.1.1
+	       {
+                criterionNumber : '11.1',
+                criterionLevel : 'IITAA10.level.A',
+                criterionDesc : 'IITAA10.description.11_1',
+                requirementUrl: '#text-equiv',
+		        rules : {
+		            // defined in text-equiv-rules.js    
+		    	    'missingsummary' : {severityCode:'level.violation', messageCode:'dataTables.msg.missingsummary'},
+		            'missingheaders' : {severityCode:'level.violation', messageCode:'dataTables.msg.missingheaders'},
+		            'duplicatesummary' : {severityCode:'level.violation', messageCode:'dataTables.msg.duplicatesummary'},
+		            'thmissingID' : {severityCode:'level.potentialViolation', messageCode:'dataTables.msg.thmissingID'},
+		            'duplicateTHEleID' : {severityCode:'level.potentialViolation', messageCode:'dataTables.msg.duplicateTHEleID'},
+		            'tdmissingheaders' : {severityCode:'level.recommendation', messageCode:'dataTables.msg.tdmissingheaders'},
+		            'invalidIDREF' : {severityCode:'level.recommendation', messageCode:'dataTables.msg.invalidIDREF'}
+            	}
+            }, // 1.3.1
+	        {
+                criterionNumber : '13.2',
+                criterionLevel : 'IITAA10.level.A',
+                criterionDesc : 'IITAA10.description.13_2',
+                requirementUrl: '#keyboard-operation',
+		        rules : {
+            		// defined in doc-structure-rules.js
+        			'missingonfocus' : {severityCode:'level.potentialViolation', messageCode:'events.msg.missingonfocus'},           		
+    				'missingonblur' : {severityCode:'level.potentialViolation', messageCode:'events.msg.missingonblur'},           		
+            		'nonfocusableonclick' : {severityCode:'level.potentialViolation', messageCode:'events.msg.nonfocusableonclick'},           		
+        			'nonfocusableonmouse' : {severityCode:'level.potentialViolation', messageCode:'events.msg.nonfocusableonmouse'},           		
+    				'selectonchange' : {severityCode:'level.recommendation', messageCode:'events.msg.selectonchange'},           		
+    				'missingkeyequiv' : {severityCode:'level.recommendation', messageCode:'events.msg.missingkeyequiv'}           		
+		    	}
+		    }, // 2.1.1 
+	        {
 	                criterionNumber : '9.2',
 	                criterionLevel : 'IITAA10.level.A',
 	                criterionDesc : 'IITAA10.description.9_2',
@@ -217,6 +264,20 @@ OpenAjax.a11y.addRuleset(
 	            		'emptyframe' : {severityCode:'level.potentialViolation', messageCode:'frames.msg.emptyframe'}           		
 			    	}
 			    }, // 2.4.1 
+			      {
+                criterionNumber : '1.3',
+                criterionLevel : 'IITAA10.level.A',
+                criterionDesc : 'IITAA10.description.1_3',
+                requirementUrl: '#navigation-mechanisms',
+		        rules : {
+            		// defined in doc-structure-rules.js
+            		'titleMissingOrEmpty' : {severityCode:'level.Violation', messageCode:'titles.msg.missingOrEmptyTitle'},
+            		'h1MissingOrEmpty' : {severityCode:'level.Violation', messageCode:'titles.msg.missingOrEmptyH1'},
+            		'h1OnlyAlt' : {severityCode:'level.PotentialViolation', messageCode:'titles.msg.H1OnlyAlt'},
+            		'titleMissingH1Words' : {severityCode:'level.PotentialViolation', messageCode:'titles.msg.titlemissingh1words'},
+            		'toomanyh1' : {severityCode:'level.PotentialViolation', messageCode:'titles.msg.toomanyh1'}
+ 		    	}
+		    }, // 2.4.2 
 	        {
 	                criterionNumber : '9.1',
 	                criterionLevel : 'IITAA10.level.A',
@@ -231,6 +292,54 @@ OpenAjax.a11y.addRuleset(
 	            		'altequaltextcontent' : {severityCode:'level.PotentialViolation', messageCode:'links.msg.altequaltextcontent'}
 	 		    	}
 			    }, //2.4.4
+			    
+		    {
+		        criterionNumber : '1.4',
+		        criterionLevel : 'IITAA10.level.AA',
+		        criterionDesc : 'IITAA10.description.1_4',
+		        requirementUrl: '#navigation-mechanisms',
+		        rules : {
+		    		// defined in doc-structure-rules.js
+		    		'headingTextMissing' : {severityCode:'level.Violation', messageCode:'headers.msg.emptyheader'},
+		    		'headingOnlyAlt' : {severityCode:'level.PotentialViolation', messageCode:'headers.msg.headerOnlyAlt'},
+		    		'headingTextLength' : {severityCode:'level.Recommendation', messageCode:'headers.msg.toolong'},
+		    		'headingImproperNesting' : {severityCode:'level.Violation', messageCode:'headers.msg.impropernesting'},
+		       		'siblingDupName' : {severityCode:'level.PotentialViolation', messageCode:'headers.msg.siblingDupName'},
+		       		'noHeaders' : {severityCode:'level.PotentialViolation', messageCode:'headers.msg.noHeaders'}
+			    }
+			}, // 2.4.6 
+			
+			{
+                criterionNumber : '2.3',
+                criterionLevel : 'IITAA10.level.A',
+                criterionDesc : 'IITAA10.description.2_3',
+                requirementUrl: '#meaning',
+		        rules : {
+            		// defined in doc-structure-rules.js
+           			'langMissing' : {severityCode:'level.Violation', messageCode:'languages.msg.langMissing'},           		
+            		'invalidLanguage' : {severityCode:'level.Violation', messageCode:'languages.msg.invalidLanguage'}           		
+		    	}
+		    }, // 3.1.1 
+			{
+		        criterionNumber : '10.1',
+		        criterionLevel : 'IITAA10.level.AA',
+		        criterionDesc : 'IITAA10.description.10_1',
+		        requirementUrl: '#minimize-error',
+		        rules : {
+		    		// defined in doc-structure-rules.js
+		    		'missinglegend' : {severityCode:'level.Violation', messageCode:'forms.msg.missinglegend'},
+		    		'encapsulatinglabel' : {severityCode:'level.Violation', messageCode:'forms.msg.encapsulatinglabel'},
+		    		'missinglabelandtitle' : {severityCode:'level.Violation', messageCode:'forms.msg.missinglabelandtitle'},
+		    		'missingaltandtitle' : {severityCode:'level.Violation', messageCode:'forms.msg.missingaltandtitle'},
+		       		'missingvalueandtitle' : {severityCode:'level.Violation', messageCode:'forms.msg.missingvalueandtitle'},
+		       		'missingbuttoncontent' : {severityCode:'level.Violation', messageCode:'forms.msg.missingbuttoncontent'},
+		       		'duplicatelabel': {severityCode:'level.Violation', messageCode:'forms.msg.duplicatelabel'},
+		       		'emptylable' : {severityCode:'level.Violation', messageCode:'forms.msg.emptylable'},
+		    		'emptylegend' : {severityCode:'level.Violation', messageCode:'forms.msg.emptylegend'},
+		    		'emptytitle' : {severityCode:'level.Violation', messageCode:'forms.msg.emptytitle'},
+		    		'duplicateFormEleID' : {severityCode:'level.PotentialViolation', messageCode:'forms.msg.duplicateFormEleID'} 
+			    }
+			}, // 3.3.2 
 	        ] 
 });
 
