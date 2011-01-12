2bf534c9c2a60d0e45ec60aa0da8c3af605ca77e
bale.prasanna <bale.prasanna@gmail.com>
2011-01-12 18:24:08 +0000

    release: ainspector version 0.90.

--- chrome/content/ainspector/OAA/scripts/util.js
+++ chrome/content/ainspector/OAA/scripts/util.js
@@ -61,16 +61,19 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 			 * default value is returned.
 			 * 
 			 *  @param node from which attributes are desired
-			 *  @param array of attributes to lok for
+			 *  @param array of attributes to look for
 			 *  @param default value if none of the specified attributes are present
 			 *  @return first nonempty attribute value or the default value
 			 */
 			getValueFromAttributes : function(node,attributeArray,defaultValue) {
 				var result = null;   
 				for (var i=0; !result && i < attributeArray.length; i++) {
-				      if (node.hasAttribute && node.hasAttribute(attributeArray[i])) {
-				         var value = node.getAttribute(attributeArray[i]);
-				         result = value.normalizeSpacing();
+				      if (OpenAjax.a11y.xbrowser.hasAttribute(node, attributeArray[i])) {
+				    	  var value = node.getAttribute(attributeArray[i]);
+				    	  if (typeof(value) != "string") {
+				    		  value = value.toString();
+				    	  }
+				    	  result = value.normalizeSpacing();
 				      }
 				   }
 				   return result || defaultValue;
@@ -134,14 +137,19 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 					for (i = 0; i < buttonNode.childNodes.length && aLabel.length == 0; i++){
 						var myName = buttonNode.childNodes[i].nodeName;
 						if (buttonNode.childNodes[i].nodeName.toLowerCase()== '#text')	{ //3/4/08 SMF Use content first
-							if (buttonNode.childNodes[i].textContent) { 
-								aLabel += buttonNode.childNodes[i].textContent.normalizeSpacing();
-								if (aLabel.length > 0) {fromSrc = "forms.source.content";aLabel += ' '}
+							if (OpenAjax.a11y.xbrowser.getTextContent(buttonNode.childNodes[i])) { 
+								aLabel += OpenAjax.a11y.xbrowser.getTextContent(buttonNode.childNodes[i]).normalizeSpacing();
+								if (aLabel.length > 0) {
+									fromSrc = "forms.source.content";
+									aLabel += ' ';}
 							}	
 						}
 						if (buttonNode.childNodes[i].nodeName.toLowerCase()== 'img')	{
 							aLabel += buttonNode.childNodes[i].getAttribute('alt').normalizeSpacing();
-							if (aLabel.length > 0) {if (fromSrc == "") {fromSrc = "forms.source.alt";} aLabel += ' '}
+							if (aLabel.length > 0) {if (fromSrc == "") {
+								fromSrc = "forms.source.alt";} 
+							aLabel += ' ';
+							}
 						}
 					}
 					var retStruct = { label: aLabel, source: fromSrc};
@@ -149,14 +157,23 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 					return retStruct;	       
 				},
 				
-				parseLabel : function (node) {				
-					const nsIAccessibleRetrieval = Components.interfaces.nsIAccessibleRetrieval;
+				parseLabel : function (node) {			
+		             var nsIAccessibleRetrieval = null;
+		             
+		             // TY currently we do not support this kind of check in IE
+		             if (typeof(Components) == "object" && !!Components.interfaces && !Components.interfaces.nsIAccessibleRetrieval) {
+		                 nsIAccessibleRetrieval = Components.interfaces.nsIAccessibleRetrieval;
+		             }
 					function getTextAssocWithIDsAT(node, field) {
 						try {
-							var accService = Components.classes["@mozilla.org/accessibleRetrieval;1"].getService(nsIAccessibleRetrieval);
-							var accObject = accService.getAccessibleFor(node);
-							if (accObject == null) return '*accessibleRetrieval failed'
-							return accObject[field];
+						    if (nsIAccessibleRetrieval) {
+      							var accService = Components.classes["@mozilla.org/accessibleRetrieval;1"].getService(nsIAccessibleRetrieval);
+      							var accObject = accService.getAccessibleFor(node);
+      							if (accObject != null) {
+      							  return accObject[field];
+      							}
+						    }
+						    return '*accessibleRetrieval failed';
 						} catch (exc) {
 							FBTrace.sysout(exc);
 						}
@@ -165,11 +182,11 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 					var retStruct = { label: null, source: null};
 					var emptytitle = false;
 		    		var type = '', title= '';
-		    		if (node.hasAttribute("aria-label")) { /* SMF which should take precedence aria-label or aria-labelledby? */ 
+		    		if (OpenAjax.a11y.xbrowser.hasAttribute(node, "aria-label")) { /* SMF which should take precedence aria-label or aria-labelledby? */ 
 		    			var aria = node.getAttribute('aria-label').normalizeSpacing();
 						if (aria.length > 0) { retStruct.label = aria;  retStruct.source = "forms.source.arialabel"; return retStruct; }
 		    		}
-		    		if (node.hasAttribute("aria-labelledby")) { 
+		    		if (OpenAjax.a11y.xbrowser.hasAttribute(node, "aria-labelledby")) { 
 		    			var aria = node.getAttribute('aria-labelledby').normalizeSpacing();
 						if (aria.length > 0) { 
 							var text = getTextAssocWithIDsAT(node, 'name');
@@ -181,9 +198,9 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 		    		
 					var tagName = node.tagName.toLowerCase();
 					if (node.type) type = node.type.toLowerCase();
-					if (node.hasAttribute("title")) title = node.getAttribute('title').normalizeSpacing();
+					if (OpenAjax.a11y.xbrowser.hasAttribute(node, "title")) title = node.getAttribute('title').normalizeSpacing();
 					if (tagName == 'input' || tagName.indexOf('select') >= 0 || tagName == 'textarea' || tagName == 'button') {
-						if (node.hasAttribute('title') && title.length == 0) { emptytitle = true; retStruct.source ="forms.source.title";}
+						if (OpenAjax.a11y.xbrowser.hasAttribute(node, 'title') && title.length == 0) { emptytitle = true; retStruct.source ="forms.source.title";}
 					}
 					if (tagName == 'button') {
 						retStruct = this.parseLabelFromButton(node);
@@ -191,8 +208,8 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 					}
 					if (tagName == 'input' && type == 'image') { // input <type=image>
 						if (emptytitle) return null;
-						if (node.hasAttribute('title')) { retStruct.label = title;  retStruct.source = "forms.source.title"; return retStruct; }
-						if (node.hasAttribute('alt')) { 
+						if (OpenAjax.a11y.xbrowser.hasAttribute(node, 'title')) { retStruct.label = title;  retStruct.source = "forms.source.title"; return retStruct; }
+						if (OpenAjax.a11y.xbrowser.hasAttribute(node, 'alt')) { 
 							var alt = node.getAttribute('alt').normalizeSpacing(); 
 							if (alt.length > 0) { retStruct.label = alt;  retStruct.source = "forms.source.alt"; return retStruct; }
 						}
@@ -200,8 +217,8 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 					}
 					if (tagName == 'input' && (type == 'submit' || type == 'reset' || type == 'button')) {
 						if (emptytitle) return null;
-						if (node.hasAttribute('title')) { retStruct.label = title;  retStruct.source = "forms.source.title"; return retStruct; }
-						if (node.hasAttribute('value')) { 
+						if (OpenAjax.a11y.xbrowser.hasAttribute(node, 'title')) { retStruct.label = title;  retStruct.source = "forms.source.title"; return retStruct; }
+						if (OpenAjax.a11y.xbrowser.hasAttribute(node, 'value')) { 
 							var value = node.getAttribute('value').normalizeSpacing(); 
 							if (value.length > 0) { retStruct.label = value;  retStruct.source = "forms.source.value"; return retStruct; }
 						}
@@ -217,10 +234,10 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 						}
 					}
 					if ((tagName.indexOf('select') >= 0 || tagName == 'textarea')) {
-						if (node.hasAttribute('title')) { retStruct.label = title;  retStruct.source = "forms.source.title"; return retStruct; }
+						if (OpenAjax.a11y.xbrowser.hasAttribute(node, 'title')) { retStruct.label = title;  retStruct.source = "forms.source.title"; return retStruct; }
 					}
 					if (tagName == 'input' && (type == 'text' || type == 'file' || type == 'password' || type == 'checkbox'|| type == 'radio')) { 
-						if (node.hasAttribute('title')) { retStruct.label = title;  retStruct.source = "forms.source.title"; return retStruct; }
+						if (OpenAjax.a11y.xbrowser.hasAttribute(node, 'title')) { retStruct.label = title;  retStruct.source = "forms.source.title"; return retStruct; }
 					}
 					return retStruct;
 					
@@ -233,7 +250,7 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 						for (var i = 0; i < temp.childNodes.length; i++) {
 							if (temp.childNodes[i].tagName &&
 									temp.childNodes[i].tagName.toLowerCase() == 'legend') {
-								return temp.childNodes[i].textContent;
+								return OpenAjax.a11y.xbrowser.getTextContent(temp.childNodes[i]);
 							}
 						}
 					}
@@ -283,7 +300,7 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 					   if(tablenode.rows.length<2) return false;
 					   if(tablenode.rows[0].cells.length>=2 || tablenode.rows[tablenode.rows.length-1].cells.length>=2) {
 					      if (tablenode.tHead) return true; //has tHead row(s)
-						  if (tablenode.caption && tablenode.caption.textContent.normalizeSpacing() != 0) return true;
+						  if (tablenode.caption && OpenAjax.a11y.xbrowser.getTextContent(tablenode.caption).normalizeSpacing() != 0) return true;
 						  if (tablenode.summary && tablenode.summary.normalizeSpacing() != 0) return true;
 					   } 
 					   for (r=0; r < tablenode.rows.length; r++) { //contains data from tablenode.tHead.rows &  tablenode.tFoot.rows
@@ -337,9 +354,9 @@ if (typeof OpenAjax.a11y.util == "undefined") {
             	containsAriaAttrIDREF: function (obj, notused){ // can be used as a context .containsAriaAttrIDREF
             		var retArray = new Array();
             		function main(ele) {
-            			for (var j=0; j < ele.attributes.length; j++) {
+            			for (var j=0; !!ele.attributes && j < ele.attributes.length; j++) {
          	    			var attrName = ele.attributes[j].name;
-            				if (attrName.substring(0, 5) == 'aria-') {
+            				if (OpenAjax.a11y.util.isDefinedAriaAttribute(ele, attrName)) {
             	    	    	var dataTypes = OpenAjax.a11y.aria.propertyDataTypes[attrName];
              	    			if (dataTypes && dataTypes.type && (dataTypes.type == "http://www.w3.org/2001/XMLSchema#idref" || dataTypes.type == "http://www.w3.org/2001/XMLSchema#idrefs")) {      					
              	    				retArray.push(ele); 
@@ -357,12 +374,12 @@ if (typeof OpenAjax.a11y.util == "undefined") {
             		return retArray;
             	}, 
              	
-            	containsAriaAttr: function (obj, notused){ // can be used as a context .containsAriaAttrIDREF
+            	containsAriaAttr: function (obj, notused){ // can be used as a context .containsAriaAttr
             		var retArray = new Array();
              		function main(ele) {
-            			for (var j=0; j < ele.attributes.length; j++) {
+            			for (var j=0; !!ele.attributes && j < ele.attributes.length; j++) {
          	    			var attrName = ele.attributes[j].name;
-            				if (attrName.substring(0, 5) == 'aria-') {
+            				if (OpenAjax.a11y.util.isDefinedAriaAttribute(ele, attrName)) {
              	    			retArray.push(ele); 
                     			j = ele.attributes.length;
             				}
@@ -384,7 +401,7 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 						var targetArray = targetids.split(" ");
 						for(var i=0; i< targetArray.length; i++) {
 		    	            var xp = "//*[@id='" + targetArray[i] + "']";  
-		    	            var xpathResult = node.evaluate(xp, node, this.defaultNSResolver, XPathResult.ANY_TYPE, null);
+		    	            var xpathResult = OpenAjax.a11y.xpath.evaluate(xp, node, this.defaultNSResolver, OpenAjax.a11y.xpath.XPathResult.ANY_TYPE, null);
 		        	    	var r = xpathResult.iterateNext();
 		        	    	if (!r) returnnotfoundids += targetArray[i] + ', ';
 						}
@@ -406,23 +423,28 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 							"mousedown","mouseup","mousemove","mouseout","mouseover","resize","load", "change");
 					else _events = targetEvents;
 					var eventNames = new Array();
-					var listener = Components.classes["@mozilla.org/eventlistenerservice;1"];
+					var listener = null;
+					
+					// TY currently we do not support this kind of check in IE
+					if (typeof(Components) == "object" && Components.classes) {
+					    listener = Components.classes["@mozilla.org/eventlistenerservice;1"];
+					}
 					if (listener != null && listener != undefined && node.tagName) {
 						var els = listener.createInstance(Components.interfaces.nsIEventListenerService); 
 			    		var infos = els.getListenerInfoFor(node, {});
 					
 						for (var i = 0; i < infos.length; ++i) {
 							var info = infos[i].QueryInterface(Components.interfaces.nsIEventListenerInfo);
-							for (j = 0; j < _events.length; j ++) if (info.type == _events[j] && !info.inSystemEventGroup && info.allowsUntrusted) eventNames.push(_events[j]); 
+							for (var j = 0; j < _events.length; j ++) if (info.type == _events[j] && !info.inSystemEventGroup && info.allowsUntrusted) eventNames.push(_events[j]); 
 						}
 					} else {  // does not pick up dynamically added events
 						if(node.getAttribute) {
 							for (var i = 0; i < _events.length; i ++) {
-								if (node.hasAttribute('on' + _events[i])) {
+								if (OpenAjax.a11y.xbrowser.hasAttribute(node, 'on' + _events[i])) {
 									for(j in _events) 	{
 										var e ='on' + _events[j];
-										if (node.hasAttribute(e)) {
-											var attrValue = GetValueFromAttributes(node,[e],"").normalizeSpacing();
+										if (OpenAjax.a11y.xbrowser.hasAttribute(node, e)) {
+											var attrValue = this.getValueFromAttributes(node,[e],"").normalizeSpacing();
 											if (attrValue != '') eventNames.push(e);
 										}
 									}
@@ -437,8 +459,8 @@ if (typeof OpenAjax.a11y.util == "undefined") {
             		var focusableElements = ['input','select','button','textarea','option','area','a', 'html']; //html is ignored since it always has keydown
              		var retArray = new Array();
              		function main(ele) {
-                   		if (focusableElements.indexOf(ele.tagName.toLowerCase()) == -1) {
- 	            			var role = ele.hasAttribute('role') ? ele.getAttribute("role") : '';
+                   		if (OpenAjax.a11y.xbrowser.indexOf(focusableElements, ele.tagName.toLowerCase()) == -1) {
+ 	            			var role = OpenAjax.a11y.xbrowser.hasAttribute(ele, 'role') ? ele.getAttribute("role") : '';
 	            			if (role == '') {
 	            				var eventNames = OpenAjax.a11y.util.getEvents(ele);
 	            				if (eventNames.length > 0) retArray.push(ele); 
@@ -458,15 +480,15 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 					var focusableElements = ['input','select','button','textarea','option','area'];
 					if (node.nodeName.toLowerCase() == "a" && typeof node.href != "undefined") return true;
 					if (node.nodeName.toLowerCase() == "area" && typeof node.href != "undefined") return true;
-					if (node.hasAttribute('tabindex') && node.getAttribute('tabindex').isInteger()) return true;
-					if (focusableElements.indexOf(node.nodeName.toLowerCase()) != -1) return true;		
+					if (OpenAjax.a11y.xbrowser.hasAttribute(node, 'tabindex') && node.getAttribute('tabindex').isInteger()) return true;
+					if (OpenAjax.a11y.xbrowser.indexOf(focusableElements, node.nodeName.toLowerCase()) != -1) return true;		
 					return false;
 				},
 
 				elementsContainingEvents: function(obj, targetEvents, needFocusable) {		
               		var retArray = new Array();
              		function main(ele) {
-             			if (typeof needFocusable === undefined || OpenAjax.a11y.util.is_focusable(ele) == needFocusable) {
+             			if (typeof(needFocusable) == "undefined" || OpenAjax.a11y.util.is_focusable(ele) == needFocusable) {
               				var eventNames = OpenAjax.a11y.util.getEvents(ele, targetEvents);
               				if (eventNames.length > 0) retArray.push(ele);
              			}
@@ -476,7 +498,7 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 	            		for (var i=0; i < docElements.length; i++) {
 	            			main(docElements[i]);
 	            		}
-	            	} else  main(obj); 
+	            	} else  main(obj);
             		return retArray;
 				},
 				
@@ -507,7 +529,7 @@ if (typeof OpenAjax.a11y.util == "undefined") {
              		return retArray;
 				},
 				*/
-				selectOnchange: function(obj, notused) {		// can be used as a context .mouseupDownMove
+				selectOnchange: function(obj, notused) {		// can be used as a context .selectOnchange
 					var targetEvents=new Array("change");
 					var retArray=new Array("change");
             		function main(ele) {
@@ -529,7 +551,8 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 				},
 
 				hasEvent : function (eventNames,a,b) {
-					if (eventNames.indexOf(a) != -1 || eventNames.indexOf('on' + a) != -1)return true;	
+					if (OpenAjax.a11y.xbrowser.indexOf(eventNames, a) != -1 ||
+							OpenAjax.a11y.xbrowser.indexOf(eventNames, ('on' + a)) != -1)return true;	
 					return false;
 				},
 
@@ -538,9 +561,20 @@ if (typeof OpenAjax.a11y.util == "undefined") {
 					if (this.hasEvent(eventNames,a) && this.hasEvent(eventNames,b)) return true;
 					else if (this.hasEvent(eventNames,'on' + a) && this.hasEvent(eventNames,'on' + b)) return true;
 					return false;
-				}
-
-			
+				},
 				
+				isDefinedAriaAttributeAtIndex : function (ele, index) {
+					var attrName = ele.attributes[index].name;
+					return this.isDefinedAriaAttribute(ele, attrName);
+				},
+				
+				isDefinedAriaAttribute : function (ele, attrName) {
+					var isDefinedAriaAttribute = false;
+					if (attrName.substring(0, 5) == 'aria-') {
+						isDefinedAriaAttribute = 
+							OpenAjax.a11y.xbrowser.hasAttribute(ele, attrName);
+					}
+					return isDefinedAriaAttribute;
+				}
 	}
 }
