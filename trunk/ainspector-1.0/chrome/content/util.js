e2f524412a7615c9e32477262ce0ce7852cd680f
bale.prasanna@gmail.com <bale.prasanna@gmail.com@ecc37590-14c7-11df-9524-c76eb015ffed>
2011-11-17 23:26:47 +0000

    version1.0

--- trunk/ainspector-1.0/chrome/content/util.js
+++ trunk/ainspector-1.0/chrome/content/util.js
@@ -1,16 +1,30 @@
     
+with (FBL) {
+
+  var ainspectorUtil = {
+	
     /**
      *  Dynamically add a stylesheet to the document.
      */
-    loadCSS = function(url, doc) {
-        if ( ! doc ) {
-            return '';
-        }
-        var newCss = doc.createElement("link");
-        newCss.rel = "stylesheet";
-        newCss.type = "text\/css";
-        newCss.href = url;
-        doc.body.appendChild(newCss);
+    loadCSS : function(url, doc) {
+      if ( ! doc ) {
+        return '';
+      }
+      var newCss = doc.createElement("link");
+      newCss.rel = "stylesheet";
+      newCss.type = "text\/css";
+      newCss.href = url;
+      doc.body.appendChild(newCss);
 
-        return newCss;
-    }  
+      return newCss;
+    },  
+
+    $HW_STR : function(name) {
+      return document.getElementById("ainspector_stringbundle").getString(name);
+    },
+
+    $HW_STRF : function(name, args) {
+      return document.getElementById("ainspector_stringbundle").getFormattedString(name, args);
+    }
+  };
+};
