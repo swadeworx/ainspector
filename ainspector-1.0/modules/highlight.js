322d6542461e8eb32235fc9271ad322a2a411d2f
bale.prasanna <bale.prasanna@gmail.com>
2012-09-26 21:23:32 +0000

    Namespace is changed from ainspector to firebug-a11y

--- ainspector-1.0/modules/highlight.js
+++ ainspector-1.0/modules/highlight.js
@@ -60,10 +60,10 @@ OAA_WEB_ACCESSIBILITY.util.highlightModule = {
    * @param {Object}  document  - Document object model of the browser view to highlight
    */
    initHighlight : function (document) {
-	
-	this.document = document;
-	
-   },	
+  
+     this.document = document;
+  
+   }, 
 
   /**
    * @function highlightCacheItems
@@ -75,105 +75,100 @@ OAA_WEB_ACCESSIBILITY.util.highlightModule = {
    * @param {Array}   items     - An array of OAA cache items to highlight
    */
    highlightCacheItems : function (items) {
-     
-     console.logStringMessage("OAA_WEB_ACCESSIBILITY.util.highlightModule.highlightCacheItems");
-     
+   
      if (this.last_highlighted_nodes && this.last_highlighted_nodes.length) {
-	
        this.removeHighlight(); 
      }
 
-    if (!items || typeof items.length == 'undefined') return;
+     if (!items || typeof items.length == 'undefined') return;
     
-    var node = null;
-    var items_len = items.length;
-    var style;
-    var tag_name;
+     var node = null;
+     var items_len = items.length;
+     var style;
+     var tag_name;
 
-    for (var i = 0; i < items_len; i++) {
-	  
-      var item = items[i];
-	  
-      if (item.dom_element) {  // item is a cache element object
-		
-		if (item.dom_element.image_only) {
-	      node = item.dom_element.node.getElementsByTagName("img")[0];
-	    }
-	    else {
-	      node = item.dom_element.node;
-	    }
-		style = item.dom_element.computed_style;
-		tag_name = item.dom_element.tag_name;
-	  }
-	  else { 
-        // If item is a dom element object or text node object
-        switch (item.type) {
+     for (var i = 0; i < items_len; i++) {
+       var item = items[i];
+    
+       if (item.dom_element) {  // item is a cache element object
+    
+         if (item.dom_element.image_only) {
+           node = item.dom_element.node.getElementsByTagName("img")[0];
+         }
+         else {
+           node = item.dom_element.node;
+         }
+         style = item.dom_element.computed_style;
+         tag_name = item.dom_element.tag_name;
+       }
+       else { 
+         // If item is a dom element object or text node object
+         switch (item.type) {
         
-        case Node.ELEMENT_NODE:
-          node = item.node;
-          style = item.computed_style;
-          tag_name = item.tag_name;
-          break;
+           case Node.ELEMENT_NODE:
+             node = item.node;
+             style = item.computed_style;
+             tag_name = item.tag_name;
+             break;
           
-        case Node.TEXT_NODE:
-	      if (item.parent_element) {
-	        node = item.parent_element.node;        
-            style = item.parent_element.computed_style;
-            tag_name = item.parent_element.tag_name;
-	      }  
-          break;
+           case Node.TEXT_NODE:
+             
+             if (item.parent_element) {
+               node = item.parent_element.node;        
+               style = item.parent_element.computed_style;
+               tag_name = item.parent_element.tag_name;
+             }  
+             break;
         
-        default: 
-          break;
-	    }
-	   
-	  }
-    console.logStringMessage("highlightModule: ", node);
+           default: 
+             break;
+         }
+     
+       }
 
-	  if (node) {
+       if (node) {
 
-//		check if the node is off screen or hidden from assistive technologies
-//		console.logStringMessage("style.visibility;" + style.visibility);
-//		console.logStringMessage("style.display;" + style.display);
+//      check if the node is off screen or hidden from assistive technologies
+//      console.logStringMessage("style.visibility;" + style.visibility);
+//      console.logStringMessage("style.display;" + style.display);
 
-		if (style.visibility == "hidden" || style.display == "none") {
-		  console.logStringMessage("node is hidden: ", node);
-		  this.isVisibletoAT(tag_name, style, node);
-		} else {
-		  node.style.outline = "medium solid red";
-		  console.logStringMessage("node is visible");
- 	      // If true, element is aligned with top of scroll area.
-	      // If false, it is aligned with bottom.
-	      node.scrollIntoView(true);
+         if (style.visibility == "hidden" || style.display == "none") {
+           this.isVisibletoAT(tag_name, style, node);
+         } else {
+           node.style.outline = "medium solid red";
 
-	      this.last_highlighted_nodes.push(node);
-		}
-	  }
-	} //end for
-  },
+//         If true, element is aligned with top of scroll area.
+//         If false, it is aligned with bottom.
+           node.scrollIntoView(true);
+
+           this.last_highlighted_nodes.push(node);
+         }
+       }
+     } //end for
+   },
   
-  /**
-   * @function removeHighlight
-   * 
-   * @memberOf OAA_WEB_ACCESSIBILITY.util.highlightModule
-   * 
-   * @desc unhighlights the nodes that are highlighted earlier and 
-   *       removes the informational message added to a page if the 
-   *       element is hidden from Assistive technlogies
-   */
+   /**
+    * @function removeHighlight
+    * 
+    * @memberOf OAA_WEB_ACCESSIBILITY.util.highlightModule
+    * 
+    * @desc unhighlights the nodes that are highlighted earlier and 
+    *       removes the informational message added to a page if the 
+    *       element is hidden from Assistive technlogies
+    */
    removeHighlight : function() {
-  
-    if (!this.last_highlighted_nodes) return;
-	
-	var length = this.last_highlighted_nodes.length;
-	var obj = this.document.getElementById("vId");
-	
-	if (obj)  this.document.body.removeChild(obj);
 
-	for (var i = 0; i < length; i++) {
-	  this.last_highlighted_nodes[i].style.outline = ""; 
-	}
-  },
+     if (!this.last_highlighted_nodes || !this.document) return;
+  
+     var length = this.last_highlighted_nodes.length;
+     var obj = this.document.getElementById("vId");
+  
+     if (obj)  this.document.body.removeChild(obj);
+
+     for (var i = 0; i < length; i++) {
+       this.last_highlighted_nodes[i].style.outline = ""; 
+     }
+   },
   
   /**
    * @function isVisibletoAT
@@ -185,14 +180,16 @@ OAA_WEB_ACCESSIBILITY.util.highlightModule = {
    * @param {Object} element
    */
   isVisibletoAT : function (element, style, node) {
-	  
-	  var new_div_element = this.document.createElement('div');
-	  var style_div = 'width:400px; padding:10px; border:3px solid blue; margin:0px; color:red; font-size:20px; position:fixed; ';
 
-	  new_div_element.id = 'vId';
-	  new_div_element.setAttribute("style", style_div);
-	  new_div_element.innerHTML = 'Element is off-screen or hidden from assistive technologies';
+     if (!this.document) return;
+
+    var new_div_element = this.document.createElement('div');
+    var style_div = 'width:400px; padding:10px; border:3px solid blue; margin:0px; color:red; font-size:20px; position:fixed; ';
+
+    new_div_element.id = 'vId';
+    new_div_element.setAttribute("style", style_div);
+    new_div_element.innerHTML = 'Element is off-screen or hidden from assistive technologies';
      
-	  this.document.body.insertBefore(new_div_element,this.document.body.childNodes[0]);
+    this.document.body.insertBefore(new_div_element,this.document.body.childNodes[0]);
   }
 };
